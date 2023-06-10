c651c48d7b36da828bb2a8381b73ab62dd02f596
Jasper-Hewitt <113954459+Jasper-Hewitt@users.noreply.github.com>
2023-06-10 12:21:41 +0800

    Add files via upload

--- wisenews_textmining.R
+++ wisenews_textmining.R
@@ -54,18 +54,25 @@ wise_df$content <- sub(".*文字快照", "", wise_df$dirty_content)
 #rid of all of the links
 wise_df$content <- stringi::stri_replace_all_regex(wise_df$content, '^[^\\p{Han}]*', "")
 
+#delete the column we no longer need
+wise_df$dirty_content <- NULL
+wise_df$dirty_date <- NULL
+
 # delete the compressed posts. these are the posts that end with . . . AND have fewer than 200 characters.
 #some posts have . . . but are not actually compressed, so we have to leave those in. 
+#first trim the data to remove any whitespaces 
+wise_df$content <- trimws(wise_df$content)
 wise_df <- wise_df[!(str_detect(wise_df$content, "\\.\\.\\.$") & nchar(wise_df$content) < 200), ]
+
+#also delete the empty or almost empty rows (some pages in the pdf only provided links to news articles)
+wise_df <- subset(wise_df, nchar(content) >= 10)
+
 
 #delete newline symbols
 wise_df$content <- gsub("\n", "", wise_df$content)
 
 
 
-#delete the column we no longer need
-wise_df$dirty_content <- NULL
-wise_df$dirty_date <- NULL
 
 
 print(wise_df$content[1])
@@ -87,9 +94,6 @@ wise_df <- wise_df %>%
   filter(str_length(content) >= 3)%>% #remove empty docs, docs with less than 3 characters
   mutate(doc_id = row_number())
 
-#check rows that were compressed
-new_df <- wise_df[nchar(wise_df$content) >200, ]
-print(new_df$content[5])
 
 
 #_______________________________________________________________________________________________________________
