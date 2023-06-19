29aa71adff1b2de88f081afb26cb62b22d67b2b6
Jasper-Hewitt <113954459+Jasper-Hewitt@users.noreply.github.com>
2023-06-19 09:56:36 +0800

    Add files via upload

--- main_code.R
+++ main_code.R
@@ -8,6 +8,13 @@ library(lubridate)
 library(purrr)
 library(readxl)
 library(ggplot2)
+library(quanteda)
+library(jiebaR)
+library(readtext)
+library(pdftools)
+library(lubridate)
+library(purrr)
+library(wordcloud2)
 setwd("/Users/jasperhewitt/Desktop/fertnews")
 
 
@@ -151,7 +158,7 @@ ggplot(by_candidate, aes(x = reorder(Candidate, avg_daily_fert_post), y = avg_da
 
 
 
-#___________#__________### 5: fertility relative to other topics (fix master to master_df!) ####___________#__________# 
+#___________#__________### 5: FERTILITY RELATIVE TO OTHER TOPICS (ELDERCARE, PUBLIC TRANSPORT, ECONOMY, TRANSPORT )####___________#__________# 
 
 #get other categories, we already have the number of posts about fertility, so we only need to get the other 4 categories
 keywords_Eldercare <- c("銀髮族", "長照", "在地安佬", "在地老化", "不老城", "重陽敬老金", "老人健保", "無障礙公車", "日照", "銀髮樂活", "敬老卡", "高齡運動")
@@ -192,7 +199,241 @@ ggplot(l_relative_importance, aes(x = reorder(topic, mentions), y = mentions)) +
   theme(text = element_text(family = "Songti SC", size=16))
 
 
+#___________#__________### 6:  MENTIONS OF SPECIFIC POLICY DIRECTIONS ####___________#__________# 
+#in this section we will look at what kind of policy directions the candidates focussed most on.
+#the charts below represent the number of mentions, not the number of posts. If a candidate mentions three different policy
+#directions within a single post, we will count the three specific policy directions that were mentioned
+#the main part of this section is to figure out how certain cities/parties mostly attempt to solve this problem.
+
+
+#___________# KEYWORD SEARCH #__________# 
+
+#define keywords for each category (based on our research into government websites, candidates' leaflets, and other sources)
+search_pattern_workplace <- "安胎假|陪產假|產檢假|育嬰留職停薪|生理假|企業托育|聯合托育|企業哺乳室"
+
+search_pattern_financial_aid <-"生育獎勵|嬰兒補助|育兒津貼|育兒補助|生養津貼|生養補助|生育津貼|生育補助|育嬰留職停薪津貼
+                                |就學補助|就寫津貼｜幼兒學前特別扣除額|免學費|育嬰留職停薪津貼|未來教育及發展帳戶"
+
+search_pattern_cat_childcare <- "公托|公幼|托育|公共托育|托育資源|居家式托育服務|機構式托育服務|收托|送托|托嬰|夜托|臨托|教保服務
+                                 |教保人員|社區保母|在校安親班|準公共機制|平價就學場域|平價名額|兒童預防保健服務0-未滿 7 歲|收出養媒合|養安置
+                                 |兒童三級預防措施|防治兒虐事件"
+
+search_pattern_cat_infertility <- "懷孕以及孕婦相關|不孕症|試管嬰兒|不孕夫|凍卵|坐月子|產檢|孕產婦關懷中心|產前健康檢查"
+
+
+#label posts according to the results of the keyword search    
+master_fert_df <- master_fert_df %>%
+  mutate(Workplace = ifelse(grepl(search_pattern_workplace, Content), 1, 0))%>%
+  mutate(Financial_aid = ifelse(grepl(search_pattern_financial_aid, Content), 1, 0))%>%
+  mutate(Childcare = ifelse(grepl(search_pattern_cat_childcare, Content), 1, 0))%>%
+  mutate(Infertility=ifelse(grepl(search_pattern_cat_infertility, Content), 1, 0))
+
+
+ 
+#___________# PLOT ALL CANDIDATES COMBINED #__________# 
+
+# Calculate the sum of the variables
+sum_df <- master_fert_df %>%
+  summarise(Workplace = sum(Workplace),
+            Financial_aid = sum(Financial_aid),
+            Childcare = sum(Childcare),
+            Infertility = sum(Infertility))
+
+# Reshape the data into a long format
+l_df <- sum_df %>%
+  gather('label', 'sum', 1:4)
+
+# Plot
+ggplot(l_df, aes(x = label, y = sum, fill = label)) + 
+  geom_bar(stat = "identity", position = "dodge", fill="lightblue") +
+#  scale_fill_manual(values = c("Workplace" = "purple", "Financial_aid" = "orange", "Childcare" = "skyblue", "Infertility" = "green")) +
+  theme_minimal() +
+  labs(title = "specific fertility policy directions mentioned \n by all candidates", x = "policy directions", y = "mentions", fill = "Label") +
+  theme_minimal() +
+  theme(text = element_text(family = "Songti SC", size = 20))
+
+
+#___________# PLOT PARTY VS PARTY#__________# 
+
+#group by party 
+by_party <- master_fert_df%>%
+  group_by(Party)%>%
+  summarise(Workplace = sum(Workplace),
+            Financial_aid = sum(Financial_aid),
+            Childcare = sum(Childcare),
+            Infertility=sum(Infertility)
+  )
+by_party$Total <- rowSums(by_party[2:5])
+
+l_by_party<-by_party%>%
+  gather('label', 'sum', 2:5)
+
+# Plot
+ggplot(l_by_party, aes(x = reorder(Party, Total), y = sum, fill = label)) + 
+  geom_bar(stat = "identity", position = "dodge") +
+  scale_fill_manual(values = c("Workplace" = "darkgreen", "Financial_aid" = "orange", "Childcare" = "blue", "Infertility" = "lightblue")) +
+  scale_x_discrete(labels = c("民眾黨" = "TPP", "無黨籍" = "IND", "國民黨" = "KMT", "民進黨" = "DPP")) +
+  theme_minimal() +
+  labs(title = 'specific fertility policy directions mentioned \n per party', x = "policy directions", y = "mentions", fill = "Label") +
+  theme(text = element_text(family = "Songti SC", size=20))
 
 
 
+#___________#PLOT CITY VS CITY #__________# 
 
+#put Chinese and english name for each city together
+master_fert_df <- master_fert_df %>%
+  mutate(City = paste(City, City_eng, sep = "\n"))
+
+#group by city
+by_city <- master_fert_df%>%
+  group_by(City)%>%
+  summarise(Workplace = sum(Workplace),
+            Financial_aid = sum(Financial_aid),
+            Childcare = sum(Childcare),
+            Infertility=sum(Infertility)
+  )
+
+# Create a column with the total mentions for each candidate
+by_city$Total <- rowSums(by_city[2:5])
+
+
+l_by_city<-by_city%>%
+  gather('label', 'sum', 2:5)
+
+# plot, city vs city, from least total mentions (left), to most total mentions (right)
+#ggplot(l_by_city, aes(x = City, y = sum, fill = label)) +
+ggplot(l_by_city, aes(x = reorder(City, Total), y = sum, fill = label)) + #reorder based on total count 
+  geom_bar(stat = "identity", position = "dodge") +
+  scale_fill_manual(values = c("Workplace" = "darkgreen", "Financial_aid" = "orange", "Childcare" = "blue", "Infertility" = "lightblue")) +
+  theme_minimal() +
+  labs(title='specific fertility policy directions mentioned \n per city', x = "policy directions", y = "mentions", fill = "Label") +
+  theme_minimal() +
+  theme(text = element_text(family = "Songti SC", size=20))
+
+
+#ALTERNATIVE (STACKPLOT) CITY BY CITY
+# plot, city vs city in ordered stack plot
+#ggplot(l_by_city, aes(x = reorder(City, Total), y = sum, fill = label)) +  # sort from high to low
+#  geom_bar(stat = "identity", position = "stack") +
+#  scale_fill_manual(values = c("Workplace" = "darkgreen", "Financial_aid" = "orange", "Childcare" = "blue", "Infertility" = "lightblue")) +
+#  coord_flip() +
+#  theme_minimal() +
+#  labs(title='City vs City', x = "City", y = "Sum", fill = "Label") +
+#  theme(text = element_text(family = "Songti SC", size=20))
+
+
+#___________# (OPTIONAL) PLOT CANDIDATE VS CANDIDATE #__________# 
+
+#candidate vs candidate old version
+#group by candidate
+by_candidate <- master_fert_df%>%
+  group_by(Candidate)%>%
+  summarise(Workplace = sum(Workplace),
+            Financial_aid = sum(Financial_aid),
+            Childcare = sum(Childcare),
+            Infertility=sum(Infertility)
+  )
+
+# Create a column with the total mentions for each candidate
+by_candidate$Total <- rowSums(by_candidate[2:5])
+
+l_by_candidate<-by_candidate%>%
+  gather('label', 'sum', 2:5)
+
+# plot, all candidates
+ggplot(l_by_candidate, aes(x = reorder(Candidate, Total), y = sum, fill = label)) +  #arrange from highest total to lowest toatl
+  geom_bar(stat = "identity", position = "dodge") +
+  scale_fill_manual(values = c("Workplace" = "darkgreen", "Financial_aid" = "orange", "Childcare" = "blue", "Infertility" = "lightblue")) +
+  coord_flip() +
+  theme_minimal() +
+  labs(title="specific fertility policy directions mentioned \n per candidate", x = "candidate", y = "mentions", fill = "Label") +
+  theme_minimal() +
+  theme(text = element_text(family = "Songti SC", size=20))
+
+# (OPTIONAL) STACKPLOT
+#ggplot(l_by_candidate, aes(x = reorder(Candidate, Total), y = sum, fill = label)) + 
+#  geom_bar(stat = "identity", position = "stack") + 
+#  scale_fill_manual(values = c("Workplace" = "darkgreen", "Financial_aid" = "orange", "Childcare" = "blue", "Infertility" = "lightblue")) +
+#  coord_flip() + 
+#  theme_minimal() +
+#  labs(title="Candidate vs Candidate", x = "Candidate", y = "Sum", fill = "Label") +
+#  theme(text = element_text(family = "Songti SC", size=20))
+
+
+#___________#__________### 7:  WORDCLOUD FOR CANDIDATES FERTILITY POSTS ####___________#__________# 
+
+
+#___________# TOKENIZATION, STOPWORDS, CUSTOM DICTIONARY #__________# 
+
+#this part is based on this tutorial by NTNU: https://alvinntnu.github.io/NTNU_ENC2036_LECTURES/chinese-text-processing.html
+
+#custom dict and stopwords: https://github.com/Jasper-Hewitt/elec_fertility/tree/main/data/dict_and_stopwords
+
+#add doc_id
+master_fert_df <- master_fert_df %>% 
+  mutate(doc_id = row_number())
+
+#load stopwords 
+stopwords <- readLines("stopwords_zh_trad.txt",
+                       encoding = "UTF-8")
+
+
+#load custom dictionary 
+my_seg <- worker(bylines = T,
+                 user = "customdict.txt",
+                 symbol = T)
+
+
+## word tokenization
+master_fert_word <- master_fert_df %>%
+  unnest_tokens(
+    output = word,
+    input = Content,  # the name of the column we are plotting
+    token = function(x)
+      segment(x, jiebar = my_seg)
+  ) %>%
+  group_by(doc_id) %>%
+  mutate(word_id = row_number()) %>% # create word index within each document
+  ungroup
+
+
+
+#same custom stopwords as we use for the wisenews wordcloud
+custom_stopwords <- c("經濟", "科技", "報導", "可能", "指出", "認為", "新聞網", "國際", 
+                      "應該", "可能", "提出", "過去", "現在", "進行","今天", "相關", "社會",
+                      "議題", "很多", "undo", "需要", "需求", "已經", "目前", "今年", "透過",
+                      "地方", "沒有", "記者", "成為", "持續", "市場", "表示", "台灣", "造成",
+                      "不少", "原因", "影響", "人口","台北", "生育率", "問題", "育兒", "生育", 
+                      "少子化", "/", "10", "20", "30", "一起", "桃園", "台中", "市長", "市民", "朋友",
+                      "城市", "12", "11", "高雄", "https", "台北市", "台中市", "台南市", "高雄市", "台南")  # specific words about 生育率# specific words about 生育率
+stopwords <- c(stopwords, custom_stopwords)
+
+#___________# WORDCLOUD AND WORD FREQUENCY LIST #__________# 
+
+## create word freq list
+master_word_freq <- master_fert_word %>%
+  mutate(word = str_trim(word)) %>%  # remove whitespaces
+  filter(str_length(word) > 1, !word %in% stopwords) %>% # remove single character words and stopwords 
+  count(word) %>%
+  arrange(desc(n))
+
+
+#plot wordcloud
+master_word_freq %>%
+  filter(n > 50) %>%
+  filter(nchar(word) >= 2) %>% ## remove one character words because they are usually not really relevant
+  wordcloud2(shape = "circle", size = 0.4)
+
+
+# Select 20 most frequent words
+top_20_words <- master_word_freq %>%
+  top_n(20, n)
+
+# plot sideways word frequency list
+ggplot(top_20_words, aes(x = reorder(word, n), y = n)) + 
+  geom_bar(stat = "identity", fill = "skyblue") +
+  coord_flip() +
+  labs(x = "Words", y = "Count", title = "20 most frequent words in candidates' \n posts about fertility") +
+  theme_minimal() +
+  theme(text = element_text(family = "Songti SC", size = 20))
